e499d186ea7e2110fa69453e473a0b83fee89591
philipp <philipp.proff@gmx.de>
2024-06-19 10:15:38 +0200

    styling adjustments

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,16 +1,16 @@
 <script setup></script>
 <template>
-    <div id="footer" class="bg-gray-200 grid grid-cols-2 p-8">
+    <div id="credits" class="grid grid-cols-2 px-8 py-4 border-t border-t-gray-900 mb-[7vh]">
         <div class="flex flex-col gap-2">
-            <h1 class="text-2xl pb-4"><b>Impressum</b></h1>
-            <h1 class="text-l"><b>Concept: </b>Julinane, Louisa, Giuliana, Philipp</h1>
-            <h1 class="text-l"><b>Text & Content: </b>Julinane, Louisa</h1>
-            <h1 class="text-l"><b>Design & UI: </b>Giuliana</h1>
-            <h1 class="text-l"><b>Data Wrangling: </b> Louisa</h1>
-            <h1 class="text-l"><b>Development: </b>Philipp</h1>
+            <h1><b>Impressum</b></h1>
+            <h3 ><b>Concept: </b>Julinane, Louisa, Giuliana, Philipp</h3>
+            <h3 ><b>Text & Content: </b>Julinane, Louisa</h3>
+            <h3 ><b>Design & UI: </b>Giuliana</h3>
+            <h3 ><b>Data Wrangling: </b> Louisa</h3>
+            <h3 ><b>Development: </b>Philipp</h3>
         </div>
         <div class="flex flex-col gap-2">
-            <h1 class="text-2xl pb-4"><b>Credits</b></h1>
+            <h1><b>Credits</b></h1>
             <img src="/images/FHP_logo.svg" alt="" class="w-1/4 pb-4">
             <p>This project was done during the class <a href="https://uclab.fh-potsdam.de/mapping/" _blank class="underline">"Mapping Cities - Making Cities"</a> at FH Potsdam in the summer semester 2024. We kindly thank Marian Dörk and Hendrik Lehman for the well taught input and the support during the development of the project.</p>
         </div>
--- components/StickyMenu.vue
+++ components/StickyMenu.vue
@@ -10,23 +10,27 @@ const props = defineProps({
     }
 })
 watch(() => props.updateMenuProgress, (nv) => {
-    document.getElementById('menu'+props.updateMenuNumber).style.backgroundImage = `linear-gradient(to right, rgb(${(props.updateMenuNumber+1)*40},${(props.updateMenuNumber+1)*40},${(props.updateMenuNumber+1)*40}) 50%, #cccccc 50%)`;
+    document.getElementById('menu'+props.updateMenuNumber).style.backgroundImage = `linear-gradient(to right, rgb(${(props.updateMenuNumber+1)*120},${(props.updateMenuNumber+1)*120},${(props.updateMenuNumber+1)*120}) 50%, #fff 50%)`;
     document.getElementById('menu'+props.updateMenuNumber).style.backgroundPosition = -100-nv*100+'%';
 })
 </script>
 <template>
     <nav class="fixed bottom-0 w-[100vw]">
-        <ul class="grid grid-cols-4 text-center">
-            <li id="menu0" class="text-2xl p-2"><NuxtLink to="#intro"><b>Intro</b></NuxtLink></li>
-            <li id="menu1" class="text-2xl p-2"><NuxtLink to="#section1"><b>Section 1</b></NuxtLink></li>
-            <li id="menu2" class="text-2xl p-2"><NuxtLink to="#section2"><b>Section 2</b></NuxtLink></li>
-            <li id="menu3" class="text-2xl p-2"><NuxtLink to="#section3"><b>Section 3</b></NuxtLink></li>
+        <ul class="grid grid-cols-6 text-center">
+            <li id="menu0" ><NuxtLink to="#intro"><b>Intro</b></NuxtLink></li>
+            <li id="menu1" ><NuxtLink to="#section1"><b>Section 1</b></NuxtLink></li>
+            <li id="menu2" ><NuxtLink to="#section2"><b>Section 2</b></NuxtLink></li>
+            <li id="menu3" ><NuxtLink to="#section3"><b>Section 3</b></NuxtLink></li>
+            <li id="menu4" ><NuxtLink to="#section4"><b>Section 4</b></NuxtLink></li>
+            <li id="menu5" ><NuxtLink to="#credits"><b>Credits</b></NuxtLink></li>
+
         </ul>
     </nav>
 </template>
 <style>
 li {
-    background-color: #cccccc;
+    box-shadow: 1px 1px 5px rgba(0,0,0,0.2);
+    background-color: #fff;
     background-position: -100%;
     background-size: 200%;
 }
