9c99f463052747b626633a7fde055c8ae85ae93f
philipp <philipp.proff@gmx.de>
2024-06-19 09:00:53 +0200

    set styling

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-
   // runtime config with env variables and exposed via useRuntimeConfig()
   runtimeConfig:{},
-
-  modules: ["@nuxtjs/tailwindcss"],
+  modules: [
+    "@nuxtjs/tailwindcss"
+  ],
   router: {
     options: {
       scrollBehaviorType: 'smooth'
     }
   },
-  css: [
-    '~/assets/css/tailwind.css',
-  ],
-  nitro: {
-    prerender: {
-      routes: [
-        "/"
-      ]
-    }
-  }
+  css: ["~/assets/css/tailwind.css"]
 })
