67060c266700a95b88e6be3740973ed32cddf081
elproffesore <philipp.proff@gmx.de>
2024-08-26 10:47:25 +0200

    last update

--- src/components/GridComponent.vue
+++ src/components/GridComponent.vue
@@ -1,7 +1,7 @@
 <script setup>
 </script>
 <template>
-    <div class="gridComponent" :class="`grid grid-cols-12 mt-[25vh] md:gap-[75px] md:mt-[35vh]`">
+    <div class="gridComponent" :class="`grid grid-cols-12 mt-[20vh] md:gap-[75px] md:mt-[30vh]`">
         <slot></slot>
     </div>
 </template>
